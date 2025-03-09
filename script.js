0869ad0380b718b8165b918da65624dc82380d60
Pravesh532 <praveshpatel9569@gmail.com>
2025-03-09 11:33:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-//complete this code
-class Animal {}
+class Animal {
+    constructor(species) {  // Correct constructor name
+        this._species = species; // Use a backing property to avoid recursion
+    }
 
-class Dog extends Animal {}
+    get species() {
+        return this._species;
+    }
 
-class Cat extends Animal {}
+    makeSound() {
+        console.log(`The ${this.species} makes a sound`); // Fix method syntax and string format
+    }
+}
+
+class Dog extends Animal {
+    constructor(species) { 
+        super(species); // Call parent constructor correctly
+    }
+
+    bark() {
+        console.log("woof");
+    }
+}
+
+class Cat extends Animal {
+    constructor(species) { 
+        super(species);
+    }
+
+    purr() {
+        console.log("purr");
+    }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
